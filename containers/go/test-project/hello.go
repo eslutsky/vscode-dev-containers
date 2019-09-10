241675f3db73919b15ea234c44876e9bd22842b9
Evgeny Slutsky <eslutsky@redhat.com>
2019-09-10 12:48:17 +0300

    first attmept

--- containers/go/test-project/hello.go
+++ containers/go/test-project/hello.go
@@ -2,9 +2,18 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
  *-------------------------------------------------------------------------------------------------------------*/
- 
+
 package main
-import "fmt"
-func main() {
-    fmt.Println("Hello remote world!")
+import (
+    "fmt"
+    "os"
+
+)
+
+/*
+this is the main function
+*/
+func main(){
+    fmt.Println("Hi there !")
+    fmt.Println( os.Getenv("PATH") )
 }
